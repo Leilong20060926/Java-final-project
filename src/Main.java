fc176bd47eec438f642a8b314263ce09467fbe67
Leilong20060926 <leilong925@gmail.com>
2025-11-26 11:06:41 +0800

    add Minesweeper.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import game.test;
-import game.test2;
+import game.Minesweeper;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Project");
-        test2 t2 = new test2();
-        t2.toString();
-        System.out.println(t2.toString());
+        System.out.println("Project\n");
+        Minesweeper minesweeper=new Minesweeper();
+        minesweeper.startGame();
     }
 }
