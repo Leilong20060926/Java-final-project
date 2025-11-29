26eacb9776fb024a5d0e78076f03922838cb5f20
Iestin520 <Iestins520@gmail.com>
2025-11-29 22:46:28 +0800

    新增遊戲化顯示方式的工具(字與字之間印出來有延遲)，如過要用，請使用(GamePrinter.printSlow)取代(system.out.println)

---
+++ src/game/extention/GamePrinter.java
@@ -0,0 +1,34 @@
+package game.extention;
+
+public class GamePrinter {
+    // 設定每個字元之間的延遲時間（毫秒）
+    private static int textDelay = 35;
+
+    public static void setTextDelay(int ms) {
+        textDelay = ms;
+    }
+
+    public static void printSlow(String text) {
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(textDelay);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println();
+    }
+
+    // 印出不換行的慢速文字
+    public static void printSlowRaw(String text) {
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(textDelay);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
