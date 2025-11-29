5aec514c155666f8669fecfd0d5cc122f11953f4
Iestin520 <Iestins520@gmail.com>
2025-11-29 22:13:57 +0800

    重寫子彈裝填機制以避免一開始的重槍機率過大

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 import game.Minesweeper;
-=======
 // import game.test;目前未用到
 import game.test2;
->>>>>>> 4794dc0 (建立 FakeBucketshotRoulette)
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Project\n");
