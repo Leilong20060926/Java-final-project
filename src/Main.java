65b8b419da836a7ca24498524460781386fcecde
Leilong20060926 <leilong925@gmail.com>
2025-12-07 22:49:52 +0800

    add

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-
 import game.Minesweeper;
-// import game.test; currently unused
-import game.test2;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Project\n");
