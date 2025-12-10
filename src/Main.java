672cd8966726b37a631d42de8f86ab40c7823e9f
9m888 <a0970772968@gmail.com>
2025-12-10 12:58:06 +0800

    add

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import game.Minesweeper;
-
+import story.Opening;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Project\n");
+        Opening opening = new Opening();
+        opening.startOpening();
         Minesweeper minesweeper=new Minesweeper();
         minesweeper.startGame();
     }
